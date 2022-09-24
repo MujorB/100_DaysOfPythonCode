a32d8224dc1798f42d8d5da48ae0508e54aaf1e1
MujorB <mujorb@gmail.com>
2022-09-24 12:14:13 +0100

    Python basics day 1of100

---
+++ Day1of100/bandname_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#--------------- A Band Name Generator Program --------------#
+
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+
+#2. Ask the user for the city that they grew up in.
+users_city = input("What's is the name of the city you grew up in? ")
+print(users_city)
+
+#3. Ask the user for the name of a pet.
+users_pet = input("What's your pet's name? ")
+print(users_pet)
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + users_city + " " + users_pet)
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
---
+++ Day1of100/print.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# The print functions is used to Output messages to the console:
+print("Hello world")
+
+# We can print strings on a New line using the \n:
+print("Hello\nHello\nHello")
+
+# We can concatenate strings like so:
+print("Hello" + " Bright")
+
+# Using the input() function
+print("Hello" + " " + input("what is your name? "))
