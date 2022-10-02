e283449d5441fcd4789e30538237b9dcaa60b9d5
MujorB <mujorb@gmail.com>
2022-10-02 11:24:59 +0100

    Functions declaration in python

---
+++ Day6of100/functions.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+# Functions in python e.g.
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+# while(condition_is_true):
+#     Do something
+#     Do something
